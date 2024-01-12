b38e2746769592039ebf33821137ac8feeb26a52
ARYAN CHAUDHARY <97864623+XERX-XD@users.noreply.github.com>
2024-01-12 21:29:21 +0545

    Create xerx.py

---
+++ xerx.py
@@ -0,0 +1,12 @@
+import os
+import platform
+print("JOIN FB GROUP")
+os.system('clear')
+os.system("git pull")
+b = platform.architecture()[0]
+if b == '64bit':
+    print(' wait some moment ')
+    
+elif b == '32bit':
+    import run
+    
